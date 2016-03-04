ac1a7e0743aebe2982385f6b38cc71e84f137767
Xavier Serrano <zombispormedio007@gmail.com>
2016-03-04 21:03:02 +0100

    commit

--- cmd/dbservice/main.go
+++ cmd/dbservice/main.go
@@ -1,32 +1,28 @@
 package main
+
 import (
-    "log"
-    "fmt"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
 )
-func determineListenAddress() (string, error) {
 
-    port := os.Getenv("PORT")
-
-
-    if port == "" {
-        port="5060"
-    }
-    return ":" + port, nil
-}
-func hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello World")
-}
 func main() {
-    addr, err := determineListenAddress()
-    if err != nil {
-        log.Fatal(err)
-    }
-    http.HandleFunc("/", hello)
-    log.Printf("Listening on %s...\n", addr)
-    if err := http.ListenAndServe(addr, nil); err != nil {
-        panic(err)
-    }
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
 }
