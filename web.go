c31ca2d5e0d5eccac4f6e16d727ca098d8899046
Xavier Serrano <zombispormedio007@gmail.com>
2016-03-04 16:48:15 +0100

    first commit

---
+++ web.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+
+)
+
+func main() {
+    http.HandleFunc("/", hello)
+    fmt.Println("listening...")
+    err := http.ListenAndServe(":5060", nil)
+    if err != nil {
+      panic(err)
+    }
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+    fmt.Fprintln(res, "hello, world")
+}
