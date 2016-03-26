b392960f34919d1e8c3b687a1ffa27c7d0974c2e
Xavier Serrano <zombispormedio007@gmail.com>
2016-03-26 16:53:44 +0100

    Cors

--- config/server-config.go
+++ config/server-config.go
@@ -3,11 +3,24 @@ package config
 import (
    
 	"github.com/gin-gonic/gin"
+    "github.com/itsjamie/gin-cors"
+    "time"
 
 )
 
 func ServerConfig(router *gin.Engine){
     router.Use(gin.Logger())
+    
+   
+    router.Use(cors.Middleware(cors.Config{
+    Origins:        "*",
+    Methods:        "GET, PUT, POST, DELETE",
+    RequestHeaders: "Origin, Authorization, Content-Type",
+    ExposedHeaders: "",
+    MaxAge: 50 * time.Second,
+    Credentials: true,
+    ValidateHeaders: false,
+}))
 
 }
 
