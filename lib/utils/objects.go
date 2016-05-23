a1954db5fe5b4ba40bfef68f64acbb98f00272d9
Xavier Serrano <zombispormedio007@gmail.com>
2016-05-23 20:25:42 +0200

    contains

--- lib/utils/objects.go
+++ lib/utils/objects.go
@@ -79,3 +79,20 @@ func ContainsObjectID(s []bson.ObjectId, e bson.ObjectId) bool {
     }
     return false
 }
+
+func Contains(array []interface{}, elem interface{})bool{
+   var contains bool
+
+	s := reflect.ValueOf(array)
+
+	for i := 0; i < s.Len(); i++ {
+		value:=s.Index(i).Interface()
+        
+        if value==elem{
+            contains=true
+            break;
+        }
+		
+	}
+    return contains
+}
